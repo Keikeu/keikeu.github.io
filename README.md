83d56769cc635449e626661cdb1a2dec01a5d530
Keikeu <karolina.m.placek@gmail.com>
2019-04-01 19:13:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Personal portfolio
+
+Made with HTML5, SCSS, JavaScript and *passion*. 
+Take a look at [the live version](https://keikeu.github.io/)
+
+### Presented projects:
+
+1. [Atrium café](https://keikeu.github.io/Atrium)
+2. [Nibble](https://keikeu.github.io/Nibble)
+3. [Peg solitaire](https://keikeu.github.io/Pegs)
+4. [Dental clinic](https://github.com/Keikeu/Dental-clinic)
+5. [Bubble sort](https://keikeu.github.io/Bubble)
---
+++ js/script.js
@@ -0,0 +1,74 @@
+const counts = document.getElementsByClassName('count__item');
+const slide = document.getElementsByClassName('slide')[0];
+const details = document.getElementsByClassName('details__item');
+let current = 0;
+
+for(let i = 0; i < counts.length; i++) {
+  counts[i].addEventListener('click', () => { changeSlide(i); });
+}
+
+document.body.addEventListener("mousewheel", MouseWheelHandler, false);
+document.body.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+
+
+function MouseWheelHandler(e)
+{
+  if(e.deltaX < 0 || e.deltaY > 0) {
+    current = (current + 1 < 5) ? ++current : current = 0;
+    changeSlide(current);
+  } else if(e.deltaX > 0 || e.deltaY < 0) {
+    current = (current - 1 >= 0) ? --current : current = 4;
+    changeSlide(current);
+  }
+
+  return false;
+}
+
+
+function changeSlide(i) {
+  animateSlide();
+  current = i;
+
+  for(let k = 0; k < details.length; k++) {
+    details[k].classList.remove('details__item--active');
+  }
+  for(let k = 0; k < counts.length; k++) {
+    counts[k].classList.remove('count__item--active');
+  }
+
+  switch (current) {
+    case (0) : document.getElementsByClassName('site-link')[0].setAttribute('href', "https://keikeu.github.io/Atrium/"); break;
+    case (1) : document.getElementsByClassName('site-link')[0].setAttribute('href', "https://keikeu.github.io/Nibble/"); break;
+    case (2) : document.getElementsByClassName('site-link')[0].setAttribute('href', "https://keikeu.github.io/Pegs/"); break;
+    case (3) : document.getElementsByClassName('site-link')[0].setAttribute('href', "https://keikeu.github.io/Clinic/"); break;
+    case (4) : document.getElementsByClassName('site-link')[0].setAttribute('href', "https://keikeu.github.io/bubble-sort/"); break;
+    default : document.getElementsByClassName('site-link')[0].setAttribute('href', "#");
+  }
+
+
+  setTimeout(function() {
+    counts[current].classList.add('count__item--active');
+    slide.style.backgroundImage = "url('img/0" + (current+1) + ".jpg')";
+  }, 500);
+
+  setTimeout(function() {
+    details[current].classList.add('details__item--active');
+  }, 600);
+}
+
+
+function animateSlide() {
+  const slideOverlay1 = document.getElementsByClassName('slide-overlay')[0];
+  const slideOverlay2 = document.getElementsByClassName('slide-overlay')[1];
+
+  slideOverlay1.classList.add('open');
+
+  setTimeout(function() {
+    slideOverlay2.classList.add('open');
+  }, 300);
+
+  setTimeout(function() {
+      slideOverlay1.classList.remove('open');
+      slideOverlay2.classList.remove('open');
+  }, 1300);
+}
